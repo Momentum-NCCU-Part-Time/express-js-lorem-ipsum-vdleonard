291928989abe061ce2cfb21bd5c03b7308a52d32
vanessa <NovaDReaper@yahoo.com>
2024-01-18 12:44:56 -0500

    added routs for get requests

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,36 @@
-const express = require('express')
+const express = require("express")
+const LoremIpsum = require("lorem-ipsum").LoremIpsum
 
 const app = express()
-const cors = require('cors')
+const cors = require("cors")
 const config = { port: process.env.PORT || 3000 }
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 10,
+    min: 5
+  },
+  wordsPerSentence: {
+    max: 20,
+    min: 5
+  }
+})
 
 // middleware
 app.use(cors())
 
 // your API route(s) here
 
+app.get("/lorem/", (req, res) => {
+  res.json({ lorem: `lorem: ${lorem.generateSentences(2)}` })
+})
 
-app.get('*', function (req, res) {
-  res.status(404).json({ error: 'route not found' })
+app.get("/lorem/:number", (req, res) => {
+  res.json({ lorem: lorem.generateParagraphs(req.params.number) })
+})
+
+app.get("*", function (req, res) {
+  res.status(404).json({ error: "route not found" })
 })
 
 // start server
